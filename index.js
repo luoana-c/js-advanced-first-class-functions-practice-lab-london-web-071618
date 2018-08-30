7f4d497ddbe819a3cf6aafb0136e3a6c2afa56e5
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-30 08:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+const logDriverNames = function (drivers) {
+  drivers.forEach(function(driver) {
+    console.log(driver.name)
+  })
+}
+  
+function logDriversByHometown(drivers, driverLocation) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === driverLocation) {
+      console.log(driver.name)
+    }
+  })
+}
+
+
+
